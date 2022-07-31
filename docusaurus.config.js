11a9eda7490908f2249c32bdf14a1e39f1a71ba0
xILight <lolgamerHDTV@gmail.com>
2022-07-31 20:45:30 +0200

    changed - organizationName, projectName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'xiLight', // Usually your GitHub org/user name.
+    projectName: 'PolliTicketDocs', // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
@@ -38,7 +38,7 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/xiLight/PolliTicketDocs/tree/master/',
                 },
                 blog: false,
                 theme: {
